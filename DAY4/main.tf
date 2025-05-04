f6343ab8476c537a36e4489354f6254f24c584a1
truongnx-smartosc <truongnx1@smartosc.com>
2025-05-04 19:58:34 +0700

    Add BigQuery dataset and table resources to Terraform configuration

--- DAY4/main.tf
+++ DAY4/main.tf
@@ -30,10 +30,34 @@ resource "google_compute_instance" "vm_instance" {
     ssh-keys = "truongnx1:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1DNeZb+r0vjTGHphtwwnLz8GgjpTC0jLaZsTDMvW3d nguyenxuantruong@truongnguyen.local"
   }
 }
-
 resource "google_storage_bucket" "yuchi-bucket" {
   name     = "my-terraform-bucket-0393882s"  
   location = "US"
 
   uniform_bucket_level_access = true 
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "my_dataset"
+  location   = "US"
+}
+
+resource "google_bigquery_table" "sample_table" {
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  table_id   = "my_table"
+
+  schema = <<EOF
+[
+  {
+    "name": "name",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "age",
+    "type": "INTEGER",
+    "mode": "NULLABLE"
+  }
+]
+EOF
 }
